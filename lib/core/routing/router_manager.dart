4ce782ea0ca73cffbb47a4aaaf58042040e8cb31
Mona Lisa <abh.prog@gmail.com>
2025-05-19 17:32:30 +0300

    Add doumentations to the AppRouterManager and its methods

--- lib/core/routing/router_manager.dart
+++ lib/core/routing/router_manager.dart
@@ -1,26 +1,75 @@
 import 'package:flutter/material.dart' show BuildContext;
 import 'package:go_router/go_router.dart';
 
+/// A utility class for managing app navigation using named routes.
+///
+/// The [AppRouterManager] class provides static methods to simplify navigation
+/// throughout the app. It wraps common navigation actions such as pushing new
+/// routes, replacing routes, and popping routes, using the provided [BuildContext].
+///
+/// This class cannot be instantiated.
 class AppRouterManager {
+  /// Private constructor to prevent instantiation.
   AppRouterManager._();
 
+  /// Pushes a new route onto the navigation stack using the given [pageName].
+  ///
+  /// Optionally, an [extra] object can be passed to the new route.
+  ///
+  /// Example:
+  /// ```dart
+  /// AppRouterManager.push(context, '/details', extra: myData);
+  /// ```
   static void push(BuildContext context, String pageName, {Object? extra}) =>
       context.push(pageName, extra: extra);
 
+  /// Pushes a named route onto the navigation stack.
+  ///
+  /// [pageName] is the name of the route to push.
+  /// Optionally, an [extra] object can be passed to the new route.
+  ///
+  /// Example:
+  /// ```dart
+  /// AppRouterManager.pushNamed(context, 'home', extra: myData);
+  /// ```
   static void pushNamed(
     BuildContext context,
     String pageName, {
     Object? extra,
   }) => context.pushNamed(pageName, extra: extra);
 
+  /// Replaces the current route by pushing a named route and removing the previous one.
+  ///
+  /// [pageName] is the name of the route to push.
+  /// Optionally, an [extra] object can be passed to the new route.
+  ///
+  /// Example:
+  /// ```dart
+  /// AppRouterManager.pushReplacementNamed(context, 'login', extra: myData);
+  /// ```
   static void pushReplacementNamed(
     BuildContext context,
     String pageName, {
     Object? extra,
   }) => context.pushReplacementNamed(pageName, extra: extra);
 
+  /// Navigates directly to a named route, replacing the current route stack.
+  ///
+  /// [pageName] is the name of the route to navigate to.
+  /// Optionally, an [extra] object can be passed to the new route.
+  ///
+  /// Example:
+  /// ```dart
+  /// AppRouterManager.goNamed(context, 'dashboard', extra: myData);
+  /// ```
   static void goNamed(BuildContext context, String pageName, {Object? extra}) =>
       context.goNamed(pageName, extra: extra);
 
+  /// Pops the top-most route off the navigation stack.
+  ///
+  /// Example:
+  /// ```dart
+  /// AppRouterManager.pop(context);
+  /// ```
   static void pop(BuildContext context) => context.pop();
 }
